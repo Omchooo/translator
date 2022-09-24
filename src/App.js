66e26be62718f611aafc4769e152dc6a236ad8c5
Omcho <omersahmanovic@gmail.com>
2022-09-24 23:35:31 +0200

    responsivity fix

--- src/App.js
+++ src/App.js
@@ -3,17 +3,56 @@ import "./App.css";
 import languages from "./components/languages";
 
 function App() {
+  let fromName = "";
+  let fromCode = "";
+  let toName = "";
+  let toCode = "";
+
   const encodedParams = new URLSearchParams();
   const inputClick = (key, name) => {
-    encodedParams.delete("to", key);
+    encodedParams.delete("from", key);
     encodedParams.append("from", key);
     document.getElementById("in").innerHTML = name;
+    fromName = name;
+    fromCode = key;
   };
 
   const outputClick = (key, name) => {
     encodedParams.delete("to", key);
     encodedParams.append("to", key);
     document.getElementById("out").innerText = name;
+    toName = name;
+    toCode = key;
+  };
+
+  const switchLang = () => {
+    if (fromName && fromCode && toName && toCode !== "") {
+      const val1 = fromName;
+      const val2 = fromCode;
+
+      fromName = toName;
+      toName = val1;
+
+      fromCode = toCode;
+      toCode = val2;
+
+      encodedParams.delete("from", fromCode);
+      encodedParams.append("from", fromCode);
+      document.getElementById("in").innerHTML = fromName;
+      console.log("from", fromName);
+
+      encodedParams.delete("to", toCode);
+      encodedParams.append("to", toCode);
+      document.getElementById("out").innerText = toName;
+      console.log("to: ", toName);
+
+      const changedText = document.getElementById("output").innerText;
+      console.log(changedText);
+      document.getElementById("input-txt").value = changedText;
+      document.getElementById("output").innerText = "";
+
+      translate();
+    }
   };
 
   const options = {
@@ -48,17 +87,38 @@ function App() {
 
       clearInterval(interval);
     }, 2000);
-  }
+  };
 
   const clip = () => {
-  const copyText = document.getElementById("output").innerText
-  navigator.clipboard.writeText(copyText);
-  }
+    const copyText = document.getElementById("output").innerText;
+    navigator.clipboard.writeText(copyText);
+  };
 
   return (
     <div className="main-pad layout">
       <div className="main layout">
-        <div className="left">
+        <div className="left layout">
+          <button
+            type="button"
+            className="btn btn-outline-dark s-btn layout"
+            onClick={() => switchLang()}
+          >
+            <svg
+              width="20px"
+              height="20px"
+              fill="currentColor"
+              class="bi bi-arrow-left-right"
+              viewBox="0 0 16 16"
+              id="IconChangeColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M1 11.5a.5.5 0 0 0 .5.5h11.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 11H1.5a.5.5 0 0 0-.5.5zm14-7a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H14.5a.5.5 0 0 1 .5.5z"
+                id="mainIconPathAttribute"
+                fill="#737373"
+              ></path>
+            </svg>
+          </button>
           <div className="center-line"></div>
           <div className="up-content layout">
             <div className="underline layout">
@@ -77,18 +137,17 @@ function App() {
 
                 <ul className="dropdown-menu">
                   {languages.map((language, index) => (
-                      <li>
-                        <button
-                          className="dropdown-item"
-                          type="button"
-                          onClick={() =>
-                            inputClick(language.code, language.name)
-                          }
-                          key={index}
-                        >
-                          {language.name}
-                        </button>
-                      </li>
+                    <li>
+                      <button
+                        className="dropdown-item"
+                        type="button"
+                        onClick={() => inputClick(language.code, language.name)}
+                        key={index}
+                        id="from"
+                      >
+                        {language.name}
+                      </button>
+                    </li>
                   ))}
                 </ul>
               </div>
@@ -132,6 +191,7 @@ function App() {
                           outputClick(language.code, language.name)
                         }
                         key={index}
+                        id="to"
                       >
                         {language.name}
                       </button>
@@ -147,7 +207,29 @@ function App() {
               <pre id="output"></pre>
             </div>
           </div>
-          <button type="button" className="btn btn-outline-dark c-btn layout" onClick={() => clip()}><svg id="IconChangeColor"  x="0px" y="0px" viewBox="0 0 115.77 122.88"  height="20px" width="20px"><g><path class="st0" d="M89.62,13.96v7.73h12.19h0.01v0.02c3.85,0.01,7.34,1.57,9.86,4.1c2.5,2.51,4.06,5.98,4.07,9.82h0.02v0.02 v73.27v0.01h-0.02c-0.01,3.84-1.57,7.33-4.1,9.86c-2.51,2.5-5.98,4.06-9.82,4.07v0.02h-0.02h-61.7H40.1v-0.02 c-3.84-0.01-7.34-1.57-9.86-4.1c-2.5-2.51-4.06-5.98-4.07-9.82h-0.02v-0.02V92.51H13.96h-0.01v-0.02c-3.84-0.01-7.34-1.57-9.86-4.1 c-2.5-2.51-4.06-5.98-4.07-9.82H0v-0.02V13.96v-0.01h0.02c0.01-3.85,1.58-7.34,4.1-9.86c2.51-2.5,5.98-4.06,9.82-4.07V0h0.02h61.7 h0.01v0.02c3.85,0.01,7.34,1.57,9.86,4.1c2.5,2.51,4.06,5.98,4.07,9.82h0.02V13.96L89.62,13.96z M79.04,21.69v-7.73v-0.02h0.02 c0-0.91-0.39-1.75-1.01-2.37c-0.61-0.61-1.46-1-2.37-1v0.02h-0.01h-61.7h-0.02v-0.02c-0.91,0-1.75,0.39-2.37,1.01 c-0.61,0.61-1,1.46-1,2.37h0.02v0.01v64.59v0.02h-0.02c0,0.91,0.39,1.75,1.01,2.37c0.61,0.61,1.46,1,2.37,1v-0.02h0.01h12.19V35.65 v-0.01h0.02c0.01-3.85,1.58-7.34,4.1-9.86c2.51-2.5,5.98-4.06,9.82-4.07v-0.02h0.02H79.04L79.04,21.69z M105.18,108.92V35.65v-0.02 h0.02c0-0.91-0.39-1.75-1.01-2.37c-0.61-0.61-1.46-1-2.37-1v0.02h-0.01h-61.7h-0.02v-0.02c-0.91,0-1.75,0.39-2.37,1.01 c-0.61,0.61-1,1.46-1,2.37h0.02v0.01v73.27v0.02h-0.02c0,0.91,0.39,1.75,1.01,2.37c0.61,0.61,1.46,1,2.37,1v-0.02h0.01h61.7h0.02 v0.02c0.91,0,1.75-0.39,2.37-1.01c0.61-0.61,1-1.46,1-2.37h-0.02V108.92L105.18,108.92z" id="mainIconPathAttribute" fill="#737373"></path></g></svg></button>
+          <button
+            type="button"
+            className="btn btn-outline-dark c-btn layout"
+            onClick={() => clip()}
+          >
+            <svg
+              id="IconChangeColor"
+              x="0px"
+              y="0px"
+              viewBox="0 0 115.77 122.88"
+              height="20px"
+              width="20px"
+            >
+              <g>
+                <path
+                  class="st0"
+                  d="M89.62,13.96v7.73h12.19h0.01v0.02c3.85,0.01,7.34,1.57,9.86,4.1c2.5,2.51,4.06,5.98,4.07,9.82h0.02v0.02 v73.27v0.01h-0.02c-0.01,3.84-1.57,7.33-4.1,9.86c-2.51,2.5-5.98,4.06-9.82,4.07v0.02h-0.02h-61.7H40.1v-0.02 c-3.84-0.01-7.34-1.57-9.86-4.1c-2.5-2.51-4.06-5.98-4.07-9.82h-0.02v-0.02V92.51H13.96h-0.01v-0.02c-3.84-0.01-7.34-1.57-9.86-4.1 c-2.5-2.51-4.06-5.98-4.07-9.82H0v-0.02V13.96v-0.01h0.02c0.01-3.85,1.58-7.34,4.1-9.86c2.51-2.5,5.98-4.06,9.82-4.07V0h0.02h61.7 h0.01v0.02c3.85,0.01,7.34,1.57,9.86,4.1c2.5,2.51,4.06,5.98,4.07,9.82h0.02V13.96L89.62,13.96z M79.04,21.69v-7.73v-0.02h0.02 c0-0.91-0.39-1.75-1.01-2.37c-0.61-0.61-1.46-1-2.37-1v0.02h-0.01h-61.7h-0.02v-0.02c-0.91,0-1.75,0.39-2.37,1.01 c-0.61,0.61-1,1.46-1,2.37h0.02v0.01v64.59v0.02h-0.02c0,0.91,0.39,1.75,1.01,2.37c0.61,0.61,1.46,1,2.37,1v-0.02h0.01h12.19V35.65 v-0.01h0.02c0.01-3.85,1.58-7.34,4.1-9.86c2.51-2.5,5.98-4.06,9.82-4.07v-0.02h0.02H79.04L79.04,21.69z M105.18,108.92V35.65v-0.02 h0.02c0-0.91-0.39-1.75-1.01-2.37c-0.61-0.61-1.46-1-2.37-1v0.02h-0.01h-61.7h-0.02v-0.02c-0.91,0-1.75,0.39-2.37,1.01 c-0.61,0.61-1,1.46-1,2.37h0.02v0.01v73.27v0.02h-0.02c0,0.91,0.39,1.75,1.01,2.37c0.61,0.61,1.46,1,2.37,1v-0.02h0.01h61.7h0.02 v0.02c0.91,0,1.75-0.39,2.37-1.01c0.61-0.61,1-1.46,1-2.37h-0.02V108.92L105.18,108.92z"
+                  id="mainIconPathAttribute"
+                  fill="#737373"
+                ></path>
+              </g>
+            </svg>
+          </button>
         </div>
       </div>
     </div>
